2b016ddeb125e449ad90663f2aafb423b2075265
nero <artur.duchenko@gmail.com>
2018-11-05 21:59:11 +0200

    Databinding

--- app/src/main/java/com/example/nero/purse/purse/update/purse/PurseUpdateActivity.kt
+++ app/src/main/java/com/example/nero/purse/purse/update/purse/PurseUpdateActivity.kt
@@ -1,13 +1,21 @@
 package com.example.nero.purse.purse.update.purse
 
+import android.content.Intent
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.nero.purse.BR
 import com.example.nero.purse.R
+import com.example.nero.purse.databinding.ActivityPurseChangeBinding
 
 class PurseUpdateActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_purse_change)
+
+        val binding: ActivityPurseChangeBinding = DataBindingUtil.setContentView(this, R.layout.activity_purse_change)
+        binding.setVariable(BR.purse, Intent().getSerializableExtra("purse"))
+        binding.notifyChange()
     }
 }
